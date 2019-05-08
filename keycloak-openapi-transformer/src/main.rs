1ab997f8bd72d77388e81fe11d0ccccd40621074
Chris Couzens <ccouzens@gmail.com>
2019-05-08 21:18:10 +0100

    Avoid unwrapping errors by using an enum
    The next thing I wanted to do before moving on to new functionality is
    to test it.

--- keycloak-openapi-transformer/src/main.rs
+++ keycloak-openapi-transformer/src/main.rs
@@ -1,30 +1,70 @@
+use cssparser;
 use openapiv3::Info;
 use scraper::{Html, Selector};
+use selectors;
 use std::error::Error;
 
 const HTML: &str = include_str!("../../keycloak/6.0.html");
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[derive(Debug)]
+enum TransformError<'i> {
+    SelectorErr(cssparser::ParseError<'i, selectors::parser::SelectorParseErrorKind<'i>>),
+    NoFindErr,
+    JsonErr(serde_json::error::Error),
+}
+
+impl<'i> std::fmt::Display for TransformError<'i> {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            TransformError::NoFindErr => write!(f, "Could not find element"),
+            x => std::fmt::Display::fmt(x, f),
+        }
+    }
+}
+
+impl<'i> std::error::Error for TransformError<'i> {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            TransformError::SelectorErr(_) => None,
+            TransformError::NoFindErr => None,
+            TransformError::JsonErr(x) => Some(x),
+        }
+    }
+}
+
+fn main() -> Result<(), TransformError<'static>> {
     let document = Html::parse_document(HTML);
 
     let info = Info {
         title: document
-            .select(&Selector::parse("h1").unwrap())
+            .select(&Selector::parse("h1").map_err(|e| TransformError::SelectorErr(e))?)
             .next()
-            .unwrap()
+            .ok_or(TransformError::NoFindErr)?
             .text()
             .collect(),
-        description: document
-            .select(&Selector::parse("#_overview + .sectionbody > .paragraph").unwrap())
-            .next()
-            .map(|n| n.text().collect::<String>().trim().to_string()),
+        description: Some(
+            document
+                .select(
+                    &Selector::parse("#_overview + .sectionbody > .paragraph")
+                        .map_err(|e| TransformError::SelectorErr(e))?,
+                )
+                .next()
+                .ok_or(TransformError::NoFindErr)?
+                .text()
+                .collect::<String>()
+                .trim()
+                .to_string(),
+        ),
         terms_of_service: None,
         contact: None,
         license: None,
         version: document
-            .select(&Selector::parse("#_version_information + .paragraph").unwrap())
+            .select(
+                &Selector::parse("#_version_information + .paragraph")
+                    .map_err(|e| TransformError::SelectorErr(e))?,
+            )
             .next()
-            .unwrap()
+            .ok_or(TransformError::NoFindErr)?
             .text()
             .collect::<String>()
             .trim()
@@ -32,7 +72,10 @@ fn main() -> Result<(), Box<dyn Error>> {
             .collect(),
     };
 
-    println!("{}", serde_json::to_string_pretty(&info)?);
+    println!(
+        "{}",
+        serde_json::to_string_pretty(&info).map_err(|e| TransformError::JsonErr(e))?
+    );
     Ok(())
 
     // let openapi: OpenAPI = OpenAPI { openapi: String::from("3.0.0"), info:  };
