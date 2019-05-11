a4a3ff1eb1cbf8ad060b5fd58f779f97156ad557
Chris Couzens <ccouzens@gmail.com>
2019-05-11 16:17:52 +0100

    Extract info parsing to module
    This will keep the code organised.
    It will also help with tests
     - They're coming
       Next step will be to get some sort of document generated within the
       keycloak section, and the tests can compare to relevant bits.
    The error changed slightly to not inlcude the JSON bit and to have the
    selector during the NoFind case

---
+++ keycloak-openapi-transformer/src/info.rs
@@ -0,0 +1,53 @@
+use cssparser;
+use openapiv3::Info;
+use scraper::Selector;
+use selectors;
+
+#[derive(Debug)]
+pub enum TransformError {
+    SelectorErr(cssparser::ParseError<'static, selectors::parser::SelectorParseErrorKind<'static>>),
+    NoFindErr(&'static str),
+}
+
+impl<'i> std::fmt::Display for TransformError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            TransformError::NoFindErr(selector) => {
+                write!(f, "Could not find element by {}", selector)
+            }
+            x => std::fmt::Display::fmt(x, f),
+        }
+    }
+}
+
+impl std::error::Error for TransformError {}
+
+fn extract_string(
+    document: &scraper::html::Html,
+    selector: &'static str,
+) -> Result<String, TransformError> {
+    Ok(document
+        .select(&Selector::parse(selector).map_err(TransformError::SelectorErr)?)
+        .next()
+        .ok_or_else(|| TransformError::NoFindErr(selector))?
+        .text()
+        .collect::<String>()
+        .trim()
+        .to_string())
+}
+
+pub fn parse(document: scraper::html::Html) -> Result<Info, TransformError> {
+    Ok(Info {
+        title: extract_string(&document, "h1")?,
+        description: Some(extract_string(
+            &document,
+            "#_overview + .sectionbody > .paragraph",
+        )?),
+        terms_of_service: None,
+        contact: None,
+        license: None,
+        version: extract_string(&document, "#_version_information + .paragraph")?
+            .split("Version: ")
+            .collect(),
+    })
+}
--- keycloak-openapi-transformer/src/main.rs
+++ keycloak-openapi-transformer/src/main.rs
@@ -1,82 +1,13 @@
-use cssparser;
-use openapiv3::Info;
-use scraper::{Html, Selector};
-use selectors;
-use std::error::Error;
+use scraper::Html;
+use serde_json::to_string_pretty;
 
 const HTML: &str = include_str!("../../keycloak/6.0.html");
 
-#[derive(Debug)]
-enum TransformError<'i> {
-    SelectorErr(cssparser::ParseError<'i, selectors::parser::SelectorParseErrorKind<'i>>),
-    NoFindErr,
-    JsonErr(serde_json::error::Error),
-}
+mod info;
 
-impl<'i> std::fmt::Display for TransformError<'i> {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            TransformError::NoFindErr => write!(f, "Could not find element"),
-            x => std::fmt::Display::fmt(x, f),
-        }
-    }
-}
-
-impl<'i> std::error::Error for TransformError<'i> {
-    fn source(&self) -> Option<&(dyn Error + 'static)> {
-        match self {
-            TransformError::SelectorErr(_) => None,
-            TransformError::NoFindErr => None,
-            TransformError::JsonErr(x) => Some(x),
-        }
-    }
-}
-
-fn main() -> Result<(), TransformError<'static>> {
+fn main() -> Result<(), Box<std::error::Error>> {
     let document = Html::parse_document(HTML);
 
-    let info = Info {
-        title: document
-            .select(&Selector::parse("h1").map_err(|e| TransformError::SelectorErr(e))?)
-            .next()
-            .ok_or(TransformError::NoFindErr)?
-            .text()
-            .collect(),
-        description: Some(
-            document
-                .select(
-                    &Selector::parse("#_overview + .sectionbody > .paragraph")
-                        .map_err(|e| TransformError::SelectorErr(e))?,
-                )
-                .next()
-                .ok_or(TransformError::NoFindErr)?
-                .text()
-                .collect::<String>()
-                .trim()
-                .to_string(),
-        ),
-        terms_of_service: None,
-        contact: None,
-        license: None,
-        version: document
-            .select(
-                &Selector::parse("#_version_information + .paragraph")
-                    .map_err(|e| TransformError::SelectorErr(e))?,
-            )
-            .next()
-            .ok_or(TransformError::NoFindErr)?
-            .text()
-            .collect::<String>()
-            .trim()
-            .split("Version: ")
-            .collect(),
-    };
-
-    println!(
-        "{}",
-        serde_json::to_string_pretty(&info).map_err(|e| TransformError::JsonErr(e))?
-    );
+    println!("{}", to_string_pretty(&info::parse(document)?)?);
     Ok(())
-
-    // let openapi: OpenAPI = OpenAPI { openapi: String::from("3.0.0"), info:  };
 }
