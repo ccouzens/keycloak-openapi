63d0c2c55cb3a40a08c7fff5e0379613804b7d20
Chris Couzens <ccouzens@gmail.com>
2019-05-11 23:20:24 +0100

    Parse string only schema objects
    The schema module already needs a refactor.
    In particular it looks verbose and the error handling is to panic.
    
    The signature of the methods will probably change
    - instead of parsing a name, it will probably loop over the document
    sections and include everything.

---
+++ keycloak-openapi-transformer/src/components/schemas.rs
@@ -0,0 +1,92 @@
+use openapiv3::ObjectType;
+use openapiv3::Schema;
+use openapiv3::SchemaData;
+use openapiv3::SchemaKind;
+use scraper::Selector;
+
+pub fn parse_schema(document: &scraper::html::Html, schema_name: &str) -> Schema {
+    let selector = Selector::parse(&format!(
+        "#_{} + table > tbody > tr",
+        schema_name.to_ascii_lowercase()
+    ))
+    .unwrap();
+    let properties = document.select(&selector).map(|row| {
+        (
+            row.select(&Selector::parse("td:first-child strong").unwrap())
+                .next()
+                .unwrap()
+                .text()
+                .collect::<String>(),
+            openapiv3::ReferenceOr::Item(Box::new(Schema {
+                schema_data: SchemaData {
+                    nullable: false,
+                    read_only: false,
+                    write_only: false,
+                    deprecated: false,
+                    external_docs: None,
+                    example: None,
+                    title: None,
+                    description: None,
+                    discriminator: None,
+                    default: None,
+                },
+                schema_kind: SchemaKind::Type(openapiv3::Type::String(openapiv3::StringType {
+                    format: openapiv3::VariantOrUnknownOrEmpty::Empty,
+                    pattern: None,
+                    enumeration: vec![],
+                })),
+            })),
+        )
+    });
+    Schema {
+        schema_data: SchemaData {
+            nullable: false,
+            read_only: false,
+            write_only: false,
+            deprecated: false,
+            external_docs: None,
+            example: None,
+            title: None,
+            description: None,
+            discriminator: None,
+            default: None,
+        },
+        schema_kind: SchemaKind::Type(openapiv3::Type::Object(ObjectType {
+            properties: properties.collect(),
+            required: vec![],
+            additional_properties: None,
+            min_properties: None,
+            max_properties: None,
+        })),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::parse_schema;
+    use openapiv3::OpenAPI;
+    use scraper::Html;
+
+    #[test]
+    fn parses_simple_schema_as_expected() {
+        const HTML: &str = include_str!("../../../keycloak/6.0.html");
+        const JSON: &str = include_str!("../../../keycloak/6.0.json");
+        let openapi: OpenAPI = serde_json::from_str(JSON).expect("Could not deserialize example");
+        let components = openapi.components.expect("Couldn't deserialize components");
+        let address_claim_set = components
+            .schemas
+            .get("AddressClaimSet")
+            .expect("Couldn't find address claim set");
+        let address_claim_set_schema = match address_claim_set {
+            openapiv3::ReferenceOr::Item(schema) => schema,
+            openapiv3::ReferenceOr::Reference { reference: _ } => {
+                panic!("Could not extract schema")
+            }
+        };
+
+        assert_eq!(
+            address_claim_set_schema,
+            &parse_schema(&Html::parse_document(HTML), "AddressClaimSet")
+        );
+    }
+}
--- keycloak-openapi-transformer/src/main.rs
+++ keycloak-openapi-transformer/src/main.rs
@@ -5,6 +5,7 @@ use std::collections::BTreeMap;
 
 const HTML: &str = include_str!("../../keycloak/6.0.html");
 
+mod components;
 mod info;
 
 fn main() -> Result<(), Box<std::error::Error>> {
@@ -14,7 +15,26 @@ fn main() -> Result<(), Box<std::error::Error>> {
         info: info::parse(&document)?,
         servers: vec![],
         paths: BTreeMap::new(),
-        components: None,
+        components: Some(openapiv3::Components {
+            security_schemes: BTreeMap::new(),
+            responses: BTreeMap::new(),
+            parameters: BTreeMap::new(),
+            examples: BTreeMap::new(),
+            request_bodies: BTreeMap::new(),
+            headers: BTreeMap::new(),
+            schemas: [(
+                "AddressClaimSet".to_string(),
+                openapiv3::ReferenceOr::Item(components::schemas::parse_schema(
+                    &document,
+                    "AddressClaimSet",
+                )),
+            )]
+            .iter()
+            .cloned()
+            .collect(),
+            links: BTreeMap::new(),
+            callbacks: BTreeMap::new(),
+        }),
         security: vec![],
         tags: vec![],
         external_docs: None,
