87348f89bc352f5283f6c956ac2d5ca20d3863a2
Chris Couzens <ccouzens@gmail.com>
2019-05-13 13:23:06 +0100

    Parse int32 as a valid type
    It looks like the next good candidates might by int64 or boolean

--- keycloak-openapi-transformer/src/components/schemas.rs
+++ keycloak-openapi-transformer/src/components/schemas.rs
@@ -28,7 +28,32 @@ pub fn parse_schemas(
 fn parse_schema(section: scraper::element_ref::ElementRef<'_>) -> Schema {
     let row_selector = Selector::parse("table > tbody > tr").unwrap();
     let property_name_selector = Selector::parse("td:first-child strong").unwrap();
+    let type_selector = Selector::parse("td:first-child + td").unwrap();
     let properties = section.select(&row_selector).map(|row| {
+        let schema_type = match row
+            .select(&type_selector)
+            .next()
+            .unwrap()
+            .text()
+            .collect::<String>()
+            .as_str()
+        {
+            "integer(int32)" => openapiv3::Type::Integer(openapiv3::IntegerType {
+                format: openapiv3::VariantOrUnknownOrEmpty::Item(openapiv3::IntegerFormat::Int32),
+                multiple_of: None,
+                exclusive_minimum: false,
+                exclusive_maximum: false,
+                minimum: None,
+                maximum: None,
+                enumeration: vec![],
+            }),
+            _ => openapiv3::Type::String(openapiv3::StringType {
+                format: Default::default(),
+                pattern: None,
+                enumeration: vec![],
+            }),
+        };
+
         (
             row.select(&property_name_selector)
                 .next()
@@ -37,11 +62,7 @@ fn parse_schema(section: scraper::element_ref::ElementRef<'_>) -> Schema {
                 .collect::<String>(),
             openapiv3::ReferenceOr::Item(Box::new(Schema {
                 schema_data: Default::default(),
-                schema_kind: SchemaKind::Type(openapiv3::Type::String(openapiv3::StringType {
-                    format: Default::default(),
-                    pattern: None,
-                    enumeration: vec![],
-                })),
+                schema_kind: SchemaKind::Type(schema_type),
             })),
         )
     });
@@ -67,7 +88,7 @@ mod tests {
     const JSON: &str = include_str!("../../../keycloak/6.0.json");
 
     #[test]
-    fn parses_simple_schema_as_expected() {
+    fn parses_string_only_schema_as_expected() {
         let openapi: OpenAPI = serde_json::from_str(JSON).expect("Could not deserialize example");
         let components = openapi.components.expect("Couldn't deserialize components");
 
@@ -76,4 +97,18 @@ mod tests {
             parse_schemas(&Html::parse_document(HTML)).get("AccessToken-CertConf")
         );
     }
+
+    #[test]
+    fn parses_int32_only_schema_as_expected() {
+        let openapi: OpenAPI = serde_json::from_str(JSON).expect("Could not deserialize example");
+        let components = openapi.components.expect("Couldn't deserialize components");
+
+        assert_eq!(
+            components
+                .schemas
+                .get("ClientInitialAccessCreatePresentation"),
+            parse_schemas(&Html::parse_document(HTML)).get("ClientInitialAccessCreatePresentation")
+        );
+    }
+
 }
