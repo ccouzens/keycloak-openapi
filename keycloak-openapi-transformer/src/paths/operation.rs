bb30bff192bfcfc6cd17eecee14472c7f4934f4b
Chris Couzens <ccouzens@gmail.com>
2019-05-26 22:11:14 +0100

    Avoid creating summaries for all path items
    If a path item doesn't have a summary defined in the HTML, it shouldn't
    end up with one in the OpenAPI.
    
    In the HTML, usually the path is given in a pre tag and the summary in
    an initial h4. If no summary is given, then the path is given in the
    initial h4 and there is no pre tag.
    
    So to find when we don't want a summary, we check to see if there is a
    pre-tag or not.

--- keycloak-openapi-transformer/src/paths/operation.rs
+++ keycloak-openapi-transformer/src/paths/operation.rs
@@ -2,11 +2,13 @@ use scraper::Selector;
 
 pub fn parse(section: &scraper::element_ref::ElementRef<'_>) -> openapiv3::Operation {
     let summary_selector = Selector::parse("h4:first-child").unwrap();
+    let pre_path_selector = Selector::parse("pre").unwrap();
 
     openapiv3::Operation {
         summary: section
-            .select(&summary_selector)
+            .select(&pre_path_selector)
             .next()
+            .and_then(|_| section.select(&summary_selector).next())
             .map(|s| s.text().collect()),
         responses: openapiv3::Responses {
             default: None,
@@ -22,5 +24,40 @@ pub fn parse(section: &scraper::element_ref::ElementRef<'_>) -> openapiv3::Opera
             .collect(),
         },
         ..Default::default()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    const HTML: &str = include_str!("../../../keycloak/6.0.html");
+    use super::parse;
+    use scraper::Html;
+    use scraper::Selector;
+
+    #[test]
+    fn most_operations_have_summaries() {
+        const CSS_SELECTOR: &str =
+            "#_paths + .sectionbody > .sect2 > #_client_initial_access_resource + .sect3";
+        let document = Html::parse_document(&HTML);
+        let section = document
+            .select(&Selector::parse(CSS_SELECTOR).unwrap())
+            .next()
+            .unwrap();
+        assert_eq!(
+            parse(&section).summary,
+            Some("Create a new initial access token.".to_string())
+        );
+    }
+
+    #[test]
+    fn some_operations_do_not_have_summaries() {
+        const CSS_SELECTOR: &str =
+            "#_paths + .sectionbody > .sect2 > #_client_initial_access_resource + .sect3 + .sect3";
+        let document = Html::parse_document(&HTML);
+        let section = document
+            .select(&Selector::parse(CSS_SELECTOR).unwrap())
+            .next()
+            .unwrap();
+        assert_eq!(parse(&section).summary, None);
     }
 }
