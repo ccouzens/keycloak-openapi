295fb0188693ca02e15a2e5da911b9cd9e253af5
Chris Couzens <ccouzens@gmail.com>
2023-10-22 19:02:04 +0100

    Be better with summaries

--- keycloak-openapi-transformer/src/paths/operation.rs
+++ keycloak-openapi-transformer/src/paths/operation.rs
@@ -3,7 +3,7 @@ use super::response;
 use scraper::Selector;
 
 lazy_static! {
-    static ref SUMMARY_SELECTOR: Selector = Selector::parse("h4:first-child + div").unwrap();
+    static ref SUMMARY_SELECTOR: Selector = Selector::parse("h4:first-child + div.paragraph > p").unwrap();
 }
 
 pub fn parse(section: &scraper::element_ref::ElementRef<'_>) -> openapiv3::Operation {
@@ -25,40 +25,5 @@ pub fn parse(section: &scraper::element_ref::ElementRef<'_>) -> openapiv3::Opera
         parameters: parse_query_params(section),
         request_body: parse_body_param(section),
         ..Default::default()
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    const HTML: &str = include_str!("../../../keycloak/9.0.html");
-    use super::parse;
-    use scraper::Html;
-    use scraper::Selector;
-
-    #[test]
-    fn most_operations_have_summaries() {
-        const CSS_SELECTOR: &str =
-            "#_paths + .sectionbody > .sect2 > #_client_initial_access_resource + .sect3";
-        let document = Html::parse_document(&HTML);
-        let section = document
-            .select(&Selector::parse(CSS_SELECTOR).unwrap())
-            .next()
-            .unwrap();
-        assert_eq!(
-            parse(&section).summary,
-            Some("Create a new initial access token.".to_string())
-        );
-    }
-
-    #[test]
-    fn some_operations_do_not_have_summaries() {
-        const CSS_SELECTOR: &str =
-            "#_paths + .sectionbody > .sect2 > #_client_initial_access_resource + .sect3 + .sect3";
-        let document = Html::parse_document(&HTML);
-        let section = document
-            .select(&Selector::parse(CSS_SELECTOR).unwrap())
-            .next()
-            .unwrap();
-        assert_eq!(parse(&section).summary, None);
     }
 }
