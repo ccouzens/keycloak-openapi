898c6995ea3de0f84c57b8f0a54b706c1fbfa347
Erik Källberg <erik@kallberg.dev>
2023-10-29 13:44:19 +0100

    Add operation ids, tags and more
    - add contact section pointing to keycloak docs
    - remove /admin/realms prefix assume it to be base path as with
      previous versions
    - add server section with example server using base path of
      /admin/realms
    - add all tags from paths to tags section
    - generate possible operation ids and choose shortest conflict free.
      generation tries to be smart with parameters by removing them from
      the id and removing 's' suffixes from previous path segment
    - adds heck as a dependency for case conversion of operation ids
    - add response status code parsing
    - prefer description section instead of summary and
      fallback to 'Missing description' if there is none

--- keycloak-openapi-transformer/src/paths/operation.rs
+++ keycloak-openapi-transformer/src/paths/operation.rs
@@ -5,18 +5,45 @@ use scraper::Selector;
 lazy_static! {
     static ref SUMMARY_SELECTOR: Selector =
         Selector::parse("h4:first-child + div.paragraph > p").unwrap();
+    static ref STATUS_CODE_SELECTOR: Selector =
+        Selector::parse(".sect4 > .stretch .valign-top:nth-child(1) .tableblock").unwrap();
+}
+
+fn parse_status_code(status_code_str: &str) -> Option<openapiv3::StatusCode> {
+    let status_code_u16 = status_code_str.parse().ok()?;
+
+    if !(100..=599).contains(&status_code_u16) {
+        return None;
+    }
+
+    Some(openapiv3::StatusCode::Code(status_code_u16))
+}
+
+fn apply_selector(
+    section: &scraper::element_ref::ElementRef<'_>,
+    selector: &Selector,
+) -> Option<String> {
+    section
+        .select(selector)
+        .next()
+        .map(|s| s.text().collect::<String>().trim().into())
+}
+
+fn status_code(section: &scraper::element_ref::ElementRef<'_>) -> openapiv3::StatusCode {
+    match apply_selector(section, &STATUS_CODE_SELECTOR) {
+        Some(code) => parse_status_code(&code).unwrap_or(openapiv3::StatusCode::Range(2)),
+        None => openapiv3::StatusCode::Range(2),
+    }
 }
 
 pub fn parse(section: &scraper::element_ref::ElementRef<'_>) -> openapiv3::Operation {
     openapiv3::Operation {
-        summary: section
-            .select(&SUMMARY_SELECTOR)
-            .next()
-            .map(|s| s.text().collect::<String>().trim().into()),
+        description: apply_selector(section, &SUMMARY_SELECTOR)
+            .or(Some("Missing description".into())),
         responses: openapiv3::Responses {
             default: None,
             responses: [(
-                openapiv3::StatusCode::Range(2),
+                status_code(section),
                 openapiv3::ReferenceOr::Item(response::parse(section)),
             )]
             .iter()
